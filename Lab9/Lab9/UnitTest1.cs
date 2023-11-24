93042b27754dd291cf5fbdfa4195ebbc84214814
Kirill <kirillgvozd0@gmail.com>
2023-11-25 02:14:47 +0300

    Deleted some comments

--- Lab9/Lab9/UnitTest1.cs
+++ Lab9/Lab9/UnitTest1.cs
@@ -36,29 +36,22 @@ namespace Lab9
         {
             driver.Navigate().GoToUrl("https://www.apple.com/");
 
-            // Находим кнопку "Vision" в хедере и кликаем по ней
             IWebElement visionButton = driver.FindElement(By.XPath("//a[contains(text(), 'Vision')]"));
             visionButton.Click();
 
-            // Ждем некоторое время для загрузки страницы Apple Vision
             Thread.Sleep(2000);
 
-            // Находим кнопку Notify me и кликаем по ней
             IWebElement notifyMeButton = driver.FindElement(By.XPath("//button[contains(text(), 'Notify me')]"));
             notifyMeButton.Click();
 
-            // Ждем некоторое время для загрузки следующей страницы
             Thread.Sleep(2000);
 
-            // Находим поле ввода email и вводим в него ваш email
             IWebElement emailInput = driver.FindElement(By.XPath("//input[@class='form-textbox-input']"));
             emailInput.SendKeys("kirillgvozd0@gmail.com");
 
-            // Находим кнопку Notify me и кликаем по ней
             notifyMeButton = driver.FindElement(By.XPath("//button[@class='button button-super']"));
             notifyMeButton.Click();
 
-            // Ждем некоторое время для завершения действий
             Thread.Sleep(2000);
         }
     }
